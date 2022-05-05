b200f1b5cc730f38bbcf1b39b1dcbd16206ef9da
liubingwen <liubingwen@huianrong.com>
2022-05-05 11:47:04 +0800

    fix(useCountDown): 兼容处理

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,8 @@
 export const inBrowser = typeof window !== "undefined";
 export const isWindow = (val: unknown): val is Window => val === window;
-const hasRequestAnimFrame = !!requestAnimationFrame;
 export function raf(fn: FrameRequestCallback): number {
-  return hasRequestAnimFrame
-    ? requestAnimationFrame(fn)
-    : setTimeout(fn, 1000 / 60);
+  return inBrowser ? requestAnimationFrame(fn) : setTimeout(fn, 1000 / 60);
 }
 export function cancelRaf(id: number) {
-  hasRequestAnimFrame ? cancelAnimationFrame(id) : clearTimeout(id);
+  inBrowser ? cancelAnimationFrame(id) : clearTimeout(id);
 }
